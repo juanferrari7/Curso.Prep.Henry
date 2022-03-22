4dc7af92e4256821a78afb41fcf6a94165339279
Chimi <juan.ferrari304@gmail.com>
2022-03-22 17:06:31 -0300

    tarea finalizada

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -110,7 +110,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var interseccion = arreglo1.filter(function(val) {
+    return arreglo2.indexOf(val) != -1;
+  });
+  return interseccion;
 }
 
 
